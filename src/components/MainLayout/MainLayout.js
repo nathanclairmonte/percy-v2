6cc28fe100fce7c3a4a8ceee6045b7f71b708bb6
nathanclairmonte <nathanclairmonte@outlook.com>
2023-04-13 00:10:41 -0400

    added components

---
+++ src/components/MainLayout/MainLayout.js
@@ -0,0 +1,48 @@
+import Head from "next/head";
+import { useRouter } from "next/router";
+import { Navbar, Footer } from "@/components/list";
+
+const MainLayout = ({ children, ...customMeta }) => {
+    const router = useRouter();
+
+    const meta = {
+        title: "Nathan Clairmonte",
+        description: `Full-stack developer, Machine Learning Engineer, Data Scientist.`,
+        image: "/avatar.png", // remember to add our own here
+        type: "website",
+        ...customMeta,
+    };
+
+    return (
+        <div>
+            <Head>
+                <title>{meta.title}</title>
+                <meta name="robots" content="follow, index" />
+                <meta content={meta.description} name="description" />
+                <meta
+                    property="og:url"
+                    content={`https://www.nathanclairmonte.dev${router.asPath}`}
+                />
+                <link rel="canonical" href={`https://www.nathanclairmonte.dev${router.asPath}`} />
+                <meta property="og:type" content={meta.type} />
+                <meta property="og:site_name" content="Manu Arora" />
+                <meta property="og:description" content={meta.description} />
+                <meta property="og:title" content={meta.title} />
+                <meta property="og:image" content={meta.image} />
+                <meta name="twitter:card" content="summary_large_image" />
+                <meta name="twitter:site" content="@cIairmonte" />
+                <meta name="twitter:title" content={meta.title} />
+                <meta name="twitter:description" content={meta.description} />
+                <meta name="twitter:image" content={meta.image} />
+                {meta.date && <meta property="article:published_time" content={meta.date} />}
+            </Head>
+            <main className="dark:bg-gray-800 w-full">
+                <Navbar />
+                <div>{children}</div>
+                <Footer />
+            </main>
+        </div>
+    );
+};
+
+export default MainLayout;
---
+++ src/components/list.js
@@ -0,0 +1,5 @@
+export { default as Navbar } from "./Navbar/Navbar";
+export { default as Footer } from "./Footer/Footer";
+export { default as Greeting } from "./HomePage/Greeting";
+export { default as Introduction } from "./HomePage/Introduction";
+export { default as Particles } from "./HomePage/Particles";
