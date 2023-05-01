5c0eb7a21cab94016e28c6db55d1945cb220caf7
nathanclairmonte <nathanclairmonte@outlook.com>
2023-05-01 12:44:06 -0400

    added some dynamic margin-top to `PageTitle`

--- src/components/PageTitle.js
+++ src/components/PageTitle.js
@@ -1,6 +1,6 @@
 const PageTitle = ({ title }) => {
     return (
-        <h1 className="text-center text-5xl font-bold text-gray-500 dark:text-zinc-400 md:mb-6 md:text-6xl">
+        <h1 className="text-center text-5xl font-bold text-gray-500 dark:text-zinc-400 md:-mt-6 md:mb-6 md:text-6xl">
             <span className="text-[#f08057]">/</span>
             {title}
         </h1>
