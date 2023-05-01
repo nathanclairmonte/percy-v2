62f4bb2b2773bf021176f94cee8a7e87c3e3255f
nathanclairmonte <nathanclairmonte@outlook.com>
2023-05-01 18:57:05 -0400

    added vercel + google analytics

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,10 +1,33 @@
-import "@/styles/globals.css";
 import { ThemeProvider } from "next-themes";
+import { Analytics } from "@vercel/analytics/react";
+import Script from "next/script";
+import "@/styles/globals.css";
 
 export default function App({ Component, pageProps }) {
     return (
-        <ThemeProvider defaultTheme="light" attribute="class">
-            <Component {...pageProps} />
-        </ThemeProvider>
+        <>
+            <Script
+                strategy="afterInteractive"
+                src="https://www.googletagmanager.com/gtag/js?id=G-8DRPJ1KR4T"
+            />
+            <Script
+                id="google-analytics"
+                strategy="afterInteractive"
+                dangerouslySetInnerHTML={{
+                    __html: `
+                        window.dataLayer = window.dataLayer || [];
+                        function gtag(){dataLayer.push(arguments);}
+                        gtag('js', new Date());
+                        gtag('config', 'G-8DRPJ1KR4T', {
+                            page_path: window.location.pathname,
+                        });
+                    `,
+                }}
+            />
+            <ThemeProvider defaultTheme="light" attribute="class">
+                <Component {...pageProps} />
+            </ThemeProvider>
+            <Analytics />
+        </>
     );
 }
