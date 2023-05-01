f302fcbf23f32d8f4efe099f6a8eb8e7700cd4d1
nathanclairmonte <nathanclairmonte@outlook.com>
2023-04-30 20:10:02 -0400

    added `PageTitle` component

---
+++ src/components/PageTitle.js
@@ -0,0 +1,10 @@
+const PageTitle = ({ title }) => {
+    return (
+        <h1 className="text-center text-5xl font-bold text-gray-500 dark:text-zinc-400 md:mb-6 md:text-6xl">
+            <span className="text-[#f08057]">/</span>
+            {title}
+        </h1>
+    );
+};
+
+export default PageTitle;
