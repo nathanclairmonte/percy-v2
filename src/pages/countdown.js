f7ae74d77ee91d6315dce96e348952da7ea5a8a4
nathanclairmonte <nathanclairmonte@outlook.com>
2023-12-31 17:23:12 -0500

    fix hydration issue

--- src/pages/countdown.js
+++ src/pages/countdown.js
@@ -12,7 +12,7 @@ const roboto = Roboto({ weight: ["400"], subsets: ["latin"] });
 
 const Countdown = () => {
     // const newYearDate = "2024-01-01T00:00:00";
-    const newYearDate = "2023-12-31T17:13:00";
+    const newYearDate = "2023-12-31T17:26:00";
     const calculateTimeLeft = (targetDate) => {
         const difference = +new Date(targetDate) - +new Date();
         let timeLeft = {};
@@ -30,6 +30,7 @@ const Countdown = () => {
 
     const [timeLeft, setTimeLeft] = useState(calculateTimeLeft(newYearDate));
     const [isCelebration, setIsCelebration] = useState(false);
+    const [isClient, setIsClient] = useState(false);
 
     const fireworksStyles = {
         left: 0,
@@ -40,6 +41,12 @@ const Countdown = () => {
         // background: "#000"
     };
 
+    // useEffect to set isClient when client mounts
+    useEffect(() => {
+        setIsClient(true);
+    }, []);
+
+    // useEffect to update timeLeft every second
     useEffect(() => {
         const timer = setTimeout(() => {
             setTimeLeft((prevTimeLeft) => {
@@ -69,6 +76,10 @@ const Countdown = () => {
     }, [timeLeft]);
 
     const renderCountdown = () => {
+        // don't render countdown if client hasn't mounted
+        if (!isClient) return null;
+
+        // render celebration if isCelebration is true
         if (isCelebration) {
             return (
                 <div
@@ -82,6 +93,7 @@ const Countdown = () => {
             );
         }
 
+        // styles for countdown rendering
         let styleClass = "text-xl sm:text-3xl md:text-5xl text-gray-50";
         if (timeLeft.hours === 0 && timeLeft.minutes > 0) {
             styleClass = "text-3xl sm:text-5xl md:text-6xl text-gray-50";
@@ -89,8 +101,10 @@ const Countdown = () => {
             styleClass = "text-[12rem] sm:text-[14rem] md:text-[17rem] text-gray-50";
         }
 
+        // pluralize helper function
         const pluralize = (count, singular) => (count === 1 ? singular : `${singular}s`);
 
+        // render countdown
         return (
             <div className={`countdown ${styleClass} ${gothic.className}`}>
                 {timeLeft.hours > 0 && (
