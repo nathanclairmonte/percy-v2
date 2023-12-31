b0a20f71293747bc0689141b4984850cc9e192ad
nathanclairmonte <nathanclairmonte@outlook.com>
2023-12-31 15:53:04 -0500

    changed custom countdown test UI

--- src/pages/countdown.js
+++ src/pages/countdown.js
@@ -1,8 +1,9 @@
 import React, { useState, useEffect } from "react";
-import { NoNavbarLayout, PageTitle } from "@/components/list";
+import { NoNavbarLayout } from "@/components/list";
 import "@/styles/Countdown.module.css";
 
 const Countdown = () => {
+    const newYearDate = "2024-01-01T00:00:00";
     const calculateTimeLeft = (targetDate) => {
         const difference = +new Date(targetDate) - +new Date();
         let timeLeft = {};
@@ -18,22 +19,52 @@ const Countdown = () => {
         return timeLeft;
     };
 
-    const [timeLeft, setTimeLeft] = useState(calculateTimeLeft("2024-01-01T00:00:00"));
+    const [timeLeft, setTimeLeft] = useState(calculateTimeLeft(newYearDate));
     const [isCelebration, setIsCelebration] = useState(false);
-    const [customDate, setCustomDate] = useState("");
+    const [customHours, setCustomHours] = useState(0);
+    const [customMinutes, setCustomMinutes] = useState(0);
+    const [customSeconds, setCustomSeconds] = useState(0);
 
     useEffect(() => {
         const timer = setTimeout(() => {
-            const newTimeLeft = calculateTimeLeft(customDate || "2024-01-01T00:00:00");
-            setTimeLeft(newTimeLeft);
-            setIsCelebration(!newTimeLeft.hours && !newTimeLeft.minutes && !newTimeLeft.seconds);
+            setTimeLeft((prevTimeLeft) => {
+                if (
+                    prevTimeLeft.seconds > 1 ||
+                    prevTimeLeft.minutes > 0 ||
+                    prevTimeLeft.hours > 0
+                ) {
+                    const totalSeconds =
+                        prevTimeLeft.hours * 3600 +
+                        prevTimeLeft.minutes * 60 +
+                        prevTimeLeft.seconds -
+                        1;
+                    return {
+                        hours: Math.floor(totalSeconds / 3600),
+                        minutes: Math.floor((totalSeconds % 3600) / 60),
+                        seconds: totalSeconds % 60,
+                    };
+                } else {
+                    setIsCelebration(true);
+                    return prevTimeLeft;
+                }
+            });
         }, 1000);
 
         return () => clearTimeout(timer);
-    });
+    }, [timeLeft]);
 
-    const handleDateChange = (e) => {
-        setCustomDate(e.target.value);
+    const handleCustomTimeChange = () => {
+        setTimeLeft({
+            hours: parseInt(customHours, 10) || 0,
+            minutes: parseInt(customMinutes, 10) || 0,
+            seconds: parseInt(customSeconds, 10) || 0,
+        });
+        setIsCelebration(false);
+    };
+
+    const resetToLiveCountdown = () => {
+        setTimeLeft(calculateTimeLeft(newYearDate));
+        setIsCelebration(false);
     };
 
     const renderCountdown = () => {
@@ -41,36 +72,77 @@ const Countdown = () => {
             return <div className="text-3xl text-green-400">Happy New Year! 🎉🎊</div>;
         }
 
-        let styleClass = "text-2xl";
+        let styleClass = "text-2xl text-gray-100";
         if (timeLeft.hours === 0 && timeLeft.minutes < 1) {
-            styleClass = "text-3xl";
+            styleClass = "text-3xl text-gray-100";
         } else if (timeLeft.minutes === 0 && timeLeft.seconds <= 59) {
-            styleClass = "text-4xl animate-pulse";
+            styleClass = "text-4xl text-gray-100 animate-pulse";
         }
+
+        const pluralize = (count, singular) => (count === 1 ? singular : `${singular}s`);
 
         return (
             <div className={`countdown ${styleClass}`}>
-                {Object.keys(timeLeft).map((interval) => (
-                    <span key={interval}>
-                        {timeLeft[interval]} {interval}{" "}
+                {timeLeft.hours > 0 && (
+                    <span>
+                        {timeLeft.hours} {pluralize(timeLeft.hours, "hour")}{" "}
                     </span>
-                ))}
+                )}
+                {timeLeft.minutes > 0 && (
+                    <span>
+                        {timeLeft.minutes} {pluralize(timeLeft.minutes, "minute")}{" "}
+                    </span>
+                )}
+                {timeLeft.seconds > 0 && timeLeft.minutes === 0 && <span>{timeLeft.seconds}</span>}
+                {timeLeft.seconds > 0 && timeLeft.minutes > 0 && (
+                    <span>
+                        {timeLeft.seconds} {pluralize(timeLeft.seconds, "second")}
+                    </span>
+                )}
             </div>
         );
     };
 
     return (
         <NoNavbarLayout title="NYE Countdown!">
-            <PageTitle title="NYE Countdown!" />
-            <div className="flex h-full flex-col items-center justify-center border border-red-500">
+            <div className="flex h-full flex-col items-center justify-center">
                 <header className="text-xl">
                     {renderCountdown()}
-                    <input
-                        type="datetime-local"
-                        className="mt-4 rounded border border-gray-600 bg-gray-700 p-2 text-white"
-                        value={customDate}
-                        onChange={handleDateChange}
-                    />
+                    <div>
+                        <input
+                            type="number"
+                            className="mr-2 mt-4 w-20 rounded border border-gray-600 bg-gray-700 p-2 text-white"
+                            placeholder="Hours"
+                            value={customHours}
+                            onChange={(e) => setCustomHours(e.target.value)}
+                        />
+                        <input
+                            type="number"
+                            className="mt-4 w-20 rounded border border-gray-600 bg-gray-700 p-2 text-white"
+                            placeholder="Minutes"
+                            value={customMinutes}
+                            onChange={(e) => setCustomMinutes(e.target.value)}
+                        />
+                        <input
+                            type="number"
+                            className="ml-2 mt-4 w-20 rounded border border-gray-600 bg-gray-700 p-2 text-white"
+                            placeholder="Seconds"
+                            value={customSeconds}
+                            onChange={(e) => setCustomSeconds(e.target.value)}
+                        />
+                        <button
+                            className="ml-4 mt-4 rounded bg-blue-500 px-4 py-2 text-white"
+                            onClick={handleCustomTimeChange}
+                        >
+                            Start Custom Countdown
+                        </button>
+                    </div>
+                    <button
+                        className="mt-4 rounded bg-green-500 px-4 py-2 text-white"
+                        onClick={resetToLiveCountdown}
+                    >
+                        Reset to Live Countdown
+                    </button>
                 </header>
             </div>
         </NoNavbarLayout>
